b971889b7efa47472a57d1fec0345bb64f72eed4
John Garcia <166593810+johnalvarogarcia@users.noreply.github.com>
2024-04-13 17:01:27 +1200

    Create vocalcolour.js

---
+++ vocalcolour.js
@@ -0,0 +1,24 @@
+
+/* //Calling command "Recall Track Preset on Selected Track" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckfs5niws0007v110s447k1ac',
+    props: {
+        trackPresetPath: ["Avid","Track Routing","Lead Vocals"],
+    }
+}); */
+
+//Calling command "Set Track Color" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckoybhyud000eua10qlvsfnie',
+    props: {
+        colorBrightness: "Medium",
+        colorNumber: 18,
+    }
+});
+
+//Calling command "Select Track Below Originally Selected Tracks" from package "CW Pro Tools Utilities" (installed from user/pkg/version "oMDTMcbKv2OT4GaE3NFzdjMdeXj2/cky0ao8iy0008az10rv495d6p/cltjc4hdf0004c5103jsd0mop")
+sf.soundflow.runCommand({
+    commandId: 'user:claz588ty00056m10sm81r59t:ckzsx50240002zx10xqlnqyeb#ckzq2t7qk0005ap10uzdfj8df',
+    props: {}
+});
+
